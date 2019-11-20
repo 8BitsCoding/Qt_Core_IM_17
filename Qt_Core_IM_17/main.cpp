8f61c7513c88acc1b2de9ecc706c2451548472f0
kimtaehyung <taehyeong77@naver.com>
2019-11-20 14:51:56 +0900

    cc

--- Qt_Core_IM_17/main.cpp
+++ Qt_Core_IM_17/main.cpp
@@ -8,14 +8,66 @@
 #include <qdebug.h>
 
 bool put(QString path, QByteArray data, QIODevice::OpenMode mode) {
-	
+	QFile file(path);
+
+	if (!file.open(mode)) {
+		qWarning() << file.errorString();
+		return false;
+	}
+
+	qint64 bytes = file.write(data);
+
+	if (!bytes) {
+		qWarning() << file.errorString();
+	}
+	else {
+		qInfo() << "Wrote " << bytes << "to the file!";
+	}
+
+	file.flush();	// not really needed
+	file.close();
+
+	return true;
+}
+
+void write(QString path, QByteArray data) {
+	qInfo() << "Write to file";
+	if (put(path, data, QIODevice::WriteOnly)) {
+		qInfo() << "Data written to file";
+	}
+	else {
+		qWarning() << "Failed to write to file";
+	}
+}
+
+void append(QString path, QByteArray data) {
+	qInfo() << "Write to file";
+	if (put(path, data, QIODevice::Append)) {
+		qInfo() << "Data written to file";
+	}
+	else {
+		qWarning() << "Failed to write to file";
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
+	QString path = QDir::currentPath() + QDir::separator() + "test.txt";
+	qInfo() << "path: " << path;
 
+	QByteArray data("Hello World!\r\n");
+	// Windows = \r\n
+	// UNIX = \n
+
+	for (int i = 0; i < 5; i++) {
+		QString num = QString::number(i);
+		num.append(" ");
+
+		QByteArray line(num.toLatin1() + data);
+		write(path, line);
+	}
 
 	return a.exec();
 }
